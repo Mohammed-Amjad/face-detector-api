662077eb56ab0d91005de30d602fc50db404a10e
Mohammed Amjad <41646436+Mohammed-Amjad@users.noreply.github.com>
2021-05-05 18:07:05 +0530

    Db config

--- database/postgresdatabase.js
+++ database/postgresdatabase.js
@@ -3,18 +3,18 @@ const knex = require('knex');
 const pgDatabase = knex({
     client: 'pg',
     // connecting in heroko
-    // connection: {
-    //     connectionString: process.env.DATABASE_URL,
-    //     ssl: {
-    //         rejectUnauthorized: false
-    //     }
-    // }
-    // connecting in dev env
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'test'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+//     connecting in dev env
+//     connection: {
+//         host: '127.0.0.1',
+//         user: 'postgres',
+//         password: 'test',
+//         database: 'test'
     }
 });
 
